7c4616ee492a05c0087f8fd624d78ac44d5d00e9
Adam Przybylek <adam.przybylek@gmail.com>
2024-01-02 20:37:00 +0100

    A link to LNBiP added

--- sections/Program.js
+++ sections/Program.js
@@ -14,7 +14,7 @@ const Program = () => (
     <a className='target' id='program'/>
     <h2 className='display-4 anchor mb-3'>Accepted Papers</h2>
 
-	<p>The conference received 41 submissions. After a rigorous review process, 11 submissions were accepted for presentation during the conference (please refer to the <a className='u' href='https://2023.fedcsis.org/main/s3e/program'>conference program</a>) and inclusion in the proceedings under various categories outlined below.</p>
+	<p>The conference received 41 submissions. After a rigorous review process, 11 submissions were accepted for presentation during the conference (please refer to the <a className='u' href='https://2023.fedcsis.org/main/s3e/program'>conference program</a>) and inclusion in the proceedings under various categories outlined below. Additionally, revised and extended versions of papers presented at the conference were published as Springer post-proceedings: <a className='u' href='https://doi.org/10.1007/978-3-031-51075-5'>LNBIP 499</a>.</p>
 
     <h3 className='display-5 anchor mb-3'>Keynote Paper</h3>
 	<ul>
