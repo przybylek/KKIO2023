d643cabb36e7b24972ebaf98a4dd112ec9f89c45
Adam Przybylek <adam.przybylek@gmail.com>
2024-01-19 15:25:53 +0100

    An LNBiP cover added

--- sections/Program.js
+++ sections/Program.js
@@ -14,12 +14,25 @@ const Program = () => (
     <a className='target' id='program'/>
     <h2 className='display-4 anchor mb-3'>Accepted Papers</h2>
 
-	<p>The conference received 41 submissions. After a rigorous review process, 11 submissions were accepted for presentation during the conference (please refer to the <a className='u' href='https://2023.fedcsis.org/main/s3e/program'>conference program</a>) and inclusion in the proceedings under various categories outlined below. Additionally, revised and extended versions of papers presented at the conference were published as Springer post-proceedings: <a className='u' href='https://doi.org/10.1007/978-3-031-51075-5'>LNBIP 499</a>.</p>
+    <Row className="justify-content-center my-2">
+
+      <Col>
+	<p>The conference received 41 submissions. After a rigorous review process, 11 submissions were accepted for presentation during the conference (please refer to the <a className='u' href='https://2023.fedcsis.org/main/s3e/program'>conference program</a>) and inclusion in the proceedings under various categories outlined below. Additionally, revised and extended versions of papers presented at the conference were published as Springer post-proceedings: <a className='u' href='https://doi.org/10.1007/978-3-031-51075-5'>LNBIP 499</a>.</p>        
 
     <h3 className='display-5 anchor mb-3'>Keynote Paper</h3>
 	<ul>
 	  <li><a className='u' href='https://2023.fedcsis.org/speakers/JerzyNawrocki'>Towards reliable rule mining about code smells: The McPython approach</a> (<em>Maciej Ziobrowski, Mirosław Ochodek, <strong>Jerzy Nawrocki</strong> and Bartosz Walter</em>)</li>
 	</ul>
+
+      </Col>
+      <Col xs lg="2"> 
+	<a href="https://doi.org/10.1007/978-3-031-51075-5" target="_blank" rel="noopener noreferrer">
+	  <Image fluid src='/LNBIP_KKIO.png' alt='LNBIP_KKIO23'/>
+	</a>        
+      </Col>
+    </Row>
+
+
 
     <h3 className='display-5 anchor mb-3'>Full Papers</h3>
 	<ul>
